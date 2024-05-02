2de93e4e79523647abe105b66ba9ac8918107699
Joaquin Megio <megiojoaquin09@gmail.com>
2024-05-03 00:27:16 +0800

    Yippie

--- SDOIN_Code/server.js
+++ SDOIN_Code/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 require("dotenv").config();
-const collection = require("./Main/db_config.js");
+const collection = require("./db_config.js");
 const passport = require("passport");
 const expressLayouts = require("express-ejs-layouts");
 const flash = require("connect-flash");
